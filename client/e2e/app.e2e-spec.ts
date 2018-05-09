23736b192467e27d254aec75a1874a40ddf8f1bf
April Eggers <april.eggers@fdiinc.com>
2018-05-09 17:46:56 -0600

    Moved the Angular app into the client folder

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('temp App', () => {
+describe('travis-experiments App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ZipcodeComponent } from './zipcode/zipcode.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZipcodeComponent
   ],
   imports: [
     BrowserModule
